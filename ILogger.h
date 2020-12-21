e8b57ed38d6adfacf0421f70407c3ad0b384b8c3
Viktoriia <viktoryacherednichenko@gmail.com>
2020-12-21 18:01:15 +0200

    implementation

---
+++ ILogger.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <ctime>
+
+class ILogger
+{
+public:
+	virtual void Write(const std::string& message) = 0;
+	virtual ~ILogger() {};
+};
+
+class Logger : public ILogger
+{
+public:
+	void Write(const std::string& message) override {
+		m_message = message;
+		std::cout << m_message << std::endl;
+	};
+	~Logger() {};
+private:
+	std::string m_message;
+};
